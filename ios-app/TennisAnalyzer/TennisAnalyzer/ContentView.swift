59d7fd381fbb8df1502beeb3a1ec6ac34df357f7
Elvis <elvisokereke.eo@gmail.com>
2026-08-05 02:37:39 +0200

    Added Xcode Project shell

---
+++ ios-app/TennisAnalyzer/TennisAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TennisAnalyzer
+//
+//  Created by m1 on 04/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-app/TennisAnalyzer/TennisAnalyzer/TennisAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisAnalyzerApp.swift
+//  TennisAnalyzer
+//
+//  Created by m1 on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct TennisAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
